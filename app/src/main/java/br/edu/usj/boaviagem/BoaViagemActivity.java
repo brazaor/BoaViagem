d38458ba5b7eda7c46e8819ffb67bdf47e1c7f7f
Rafael Brazao <brazaor@gmail.com>
2017-10-11 21:39:34 -0300

    modificações após a aula de 11/10

--- app/src/main/java/br/edu/usj/boaviagem/BoaViagemActivity.java
+++ app/src/main/java/br/edu/usj/boaviagem/BoaViagemActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by rafael on 04/10/17.
@@ -11,13 +13,30 @@ import android.view.View;
 
 public class BoaViagemActivity extends Activity {
 
+    private EditText usuario, senha;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_login);
+        this.usuario = (EditText) findViewById(R.id.id_usuario);
+        this.senha = (EditText) findViewById(R.id.id_senha);
     }
 
     public void logar(View view) {
-        //Aqui terá a implementacao do evento onClick
+        String usuarioInformado = this.usuario.getText().toString();
+        String senhaInformada = this.senha.getText().toString();
+
+        if(usuarioInformado.equals("usj")
+                && senhaInformada.equals("usj")){
+            //efetuar login
+        }
+        else{
+            //exibir mensagem erro
+            String msg = getString(R.string.erro_autenticacao);
+            Toast t = Toast.makeText(this,msg, Toast.LENGTH_LONG);
+            t.show();
+        }
+
     }
 }
