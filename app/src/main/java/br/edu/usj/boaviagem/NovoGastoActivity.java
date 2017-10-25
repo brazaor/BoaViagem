ded392587d97373d9478efac6161ec948a71dbb1
Rafael Brazao <brazaor@gmail.com>
2017-10-25 17:48:29 -0200

    inclusão da tela de novo gasto

---
+++ app/src/main/java/br/edu/usj/boaviagem/NovoGastoActivity.java
@@ -0,0 +1,60 @@
+package br.edu.usj.boaviagem;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+
+/**
+ * Created by rafael on 25/10/17.
+ */
+
+public class NovoGastoActivity extends Activity {
+
+    private int ano, mes, dia;
+    private Button dataGasto;
+
+    private DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            ano = year;
+            mes = month;
+            dia = day;
+            dataGasto.setText(dia + "/" + (mes+1) + "/" + ano);
+        }
+    };
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_novo_gasto);
+
+        Calendar cal = Calendar.getInstance();
+        ano = cal.get(Calendar.YEAR);
+        mes = cal.get(Calendar.MONTH);
+        dia = cal.get(Calendar.DAY_OF_MONTH);
+
+        dataGasto = (Button) findViewById(R.id.id_data);
+        dataGasto.setText(dia + "/" + (mes+1) + "/" + ano);
+
+    }
+
+    public void selecionarData(View view){
+        showDialog(view.getId());
+    }
+
+    @Nullable
+    @Override
+    protected Dialog onCreateDialog(int id, Bundle args) {
+        if (R.id.id_data == id){
+            return new DatePickerDialog(this, listener, ano, mes, dia);
+        }
+        return null;
+    }
+}
